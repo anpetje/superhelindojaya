e88f28a8577e3f149b49b308ab2d78e0167ce732
zelvay <zelvayufi.zy@gmail.com>
2025-08-03 15:10:19 +0700

    feat: update HookMessage component styles for improved typography and layout responsiveness

--- component/hook-message.tsx
+++ component/hook-message.tsx
@@ -5,13 +5,16 @@ const HookMessage = () => {
   return (
     <LayoutSection
       id='hook-message'
-      classNameContainer='flex flex-col items-center justify-center text-center gap-[50px]'
+      classNameSection='bg-[#232323]'
+      classNameContainer='flex flex-col items-center justify-center text-center gap-[40px] sm:gap-[50px] container mx-auto'
     >
       <div className='flex flex-col gap-2.5'>
-        <div className={`text-[42px] text-[#C48A04] font-bold leading-[130%] uppercase `}>
+        <div
+          className={`text-2xl sm:text-[42px] text-[#C48A04] font-bold leading-[130%] uppercase `}
+        >
           Elevate Your Building with World-Class Elevator Technology
         </div>
-        <p className='text-xl text-white'>
+        <p className='text-base sm:text-xl text-white'>
           We provide premium elevator & escalator solutions for homes, offices, hospitals, malls,
           and more — Exclusive installation by PT Superhelindo Jaya, an authorized sole agent of
           Hyundai Elevator & Escalator in Indonesia.
@@ -19,7 +22,7 @@ const HookMessage = () => {
       </div>
       <Link
         href='#inquiry'
-        className={`px-6 py-3 text-white font-bold uppercase rounded-full border border-[#FCF2D9] bg-gradient-to-r from-[#008837] to-[#FFB300] hover:from-green-700 hover:to-yellow-500 transition`}
+        className={`px-6 py-3 text-white font-bold uppercase rounded-full border border-[#FCF2D9] bg-gradient-to-r from-[#008837] to-[#FFB300] hover:from-green-700 hover:to-yellow-500 transition text-sm sm:text-base`}
       >
         GET A FREE CONSULTATION, EXCLUSIVE OFFER AWAITS
       </Link>
