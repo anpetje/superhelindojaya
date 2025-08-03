53b3327cf204aad6a140e156bdf00fc70d3d8e5c
zelvay <zelvayufi.zy@gmail.com>
2025-08-03 15:40:29 +0700

    feat: enhance Footer and WebBanner components styles for improved layout and responsiveness

--- component/footer.tsx
+++ component/footer.tsx
@@ -14,16 +14,15 @@ import Fax from '@/assets/images/contact/fax.png';
 const Footer = () => {
   return (
     <footer>
-      <div className='bg-[#151515] py-[60px] px-6 md:py-[100px] md:px-10 relative'>
+      <div className='bg-[#151515] py-[40px] sm:py-[60px] px-6 md:py-[100px] md:px-10 relative'>
         <div className='container mx-auto'>
-          <div className='flex items-center justify-center gap-10 mb-10 bg-white rounded-[100px] py-9 px-[120px] '>
+          <div className='flex items-center justify-center gap-2.5 sm:gap-10 mb-10 sm:mb-[50px] bg-white/80 rounded-[100px] py-4 px-[30px] sm:py-9 sm:px-[120px] border-2 border-white backdrop-blur-[14px] '>
             <Image
               src={ImagesVariables.logoWithText.src}
               alt={ImagesVariables.logoWithText.alt}
               width={300}
               height={100}
-              className='w-auto h-[46px]'
-              priority
+              className='w-auto max-w-full h-5 sm:h-[46px]'
             />
 
             <Image
@@ -31,20 +30,20 @@ const Footer = () => {
               alt={ImagesVariables.hyundaiElevator.alt}
               width={200}
               height={100}
-              className='w-auto h-[46px]'
+              className='w-auto max-w-full h-5 sm:h-[46px]'
             />
           </div>
-          <div className='grid grid-cols-2 gap-[130px] mt-[50px]'>
-            <div className='flex flex-col gap-[30px]'>
-              <div className={`text-xl text-[#FFB300] font-bold uppercase`}>
+          <div className='grid grid-cols-1 sm:grid-cols-2 gap-[30px] sm:gap-[130px]'>
+            <div className='flex flex-col gap-2.5 sm:gap-[30px]'>
+              <div className={`text-lg sm:text-xl text-[#FFB300] font-bold uppercase`}>
                 PT Superhelindo Jaya
               </div>
-              <div className='text-xl text-white'>
+              <div className='text-base sm:text-xl text-white'>
                 We provide premium elevator & escalator solutions for homes, offices, hospitals,
                 malls, and more — Exclusive installation by PT Superhelindo Jaya, an authorized sole
                 agent of Hyundai Elevator & Escalator in Indonesia.
               </div>
-              <div className='flex gap-2.5'>
+              <div className='flex gap-2.5 mt-5 sm:mt-0'>
                 <Link href={siteConfig.links.instagram} target='_blank'>
                   <Image
                     src={Intagram}
@@ -65,14 +64,14 @@ const Footer = () => {
                 </Link>
               </div>
             </div>
-            <div className='flex flex-col gap-2.5 text-xl text-white'>
+            <div className='flex flex-col gap-2.5 text-xs sm:text-xl text-white'>
               <div>
                 <Image
                   src={Location}
                   alt='Location'
                   width={32}
                   height={32}
-                  className='inline-block mr-2.5 h-[32px]'
+                  className='inline-block mr-2.5 h-5 w-5 sm:h-8 sm:w-8'
                 />
                 {siteConfig.contact.address}
               </div>
@@ -82,7 +81,7 @@ const Footer = () => {
                   alt='Phone'
                   width={32}
                   height={32}
-                  className='inline-block mr-2.5 h-[32px]'
+                  className='inline-block mr-2.5 h-5 w-5 sm:h-8 sm:w-8'
                 />
                 {siteConfig.contact.phone} (Hunting), {siteConfig.contact.phone2}
               </div>
@@ -92,7 +91,7 @@ const Footer = () => {
                   alt='Fax'
                   width={32}
                   height={32}
-                  className='inline-block mr-2.5 h-[32px]'
+                  className='inline-block mr-2.5 h-5 w-5 sm:h-8 sm:w-8'
                 />
                 {siteConfig.contact.fax}
               </div>
@@ -102,7 +101,7 @@ const Footer = () => {
                   alt='Email'
                   width={32}
                   height={32}
-                  className='inline-block mr-2.5 h-[32px]'
+                  className='inline-block mr-2.5 h-5 w-5 sm:h-8 sm:w-8'
                 />
                 <Link href={`mailto:${siteConfig.contact.email}`} target='_blank'>
                   {siteConfig.contact.email}
