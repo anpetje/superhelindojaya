a814db27f6914b4be704999176c90f1434ca6cd8
zelvay <zelvayufi.zy@gmail.com>
2025-08-03 14:25:57 +0700

    feat: update Header component styles for improved layout and responsiveness

--- component/header.tsx
+++ component/header.tsx
@@ -3,13 +3,13 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <header className='w-full h-[100px] bg-white flex items-center justify-center'>
+    <header className='w-full h-[44px] sm:h-[100px] bg-white flex items-center justify-center  px-4 py-2.5'>
       <Image
         src={siteConfig.logo_with_text}
         alt='SuperHelindo Jaya'
         width={756}
         height={92}
-        className='h-[46px] w-auto'
+        className='h-[24px] sm:h-[46px] w-auto max-w-full'
         priority
       />
     </header>
